4a27def02f7f9fe81d918f03c98fe11bba3be956
Vinay Kokate <vinaynkokate@gmail.com>
2023-04-03 17:54:20 +0530

    automatic refresh token after expiration time using interceptors and axios instance

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,16 +1,50 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 function App() {
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
-
+  const [logout, setlogout] = useState(false);
   useEffect(() => {
     console.log(username);
     console.log(password);
   }, [username, password]);
+  const jwtaxios = axios.create();
+  jwtaxios.interceptors.request.use(
+    async (req) => {
+      let currentDate = new Date();
+      const decodedtoken = await jwt_decode(user.accesstoken);
+      if (decodedtoken.exp * 1000 < currentDate.getTime()) {
+        const data = await refreshToken();
+        req.headers["authorization"] = "bearer " + data.accesstoken;
+      }
+      return req;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const refreshToken = async () => {
+    try {
+      const res = await axios.post("http://localhost:3001/api/refresh", {
+        token: user.refreshtoken,
+      });
+      setUser({
+        ...user,
+        accesstoken: res.data.accesstoken,
+        refreshtoken: res.data.refreshtoken,
+      });
+      console.log(user);
+      console.log(jwt_decode(user.accesstoken));
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -29,7 +63,7 @@ function App() {
     setSuccess(false);
     setError(false);
     try {
-      await axios.delete(`http://localhost:3001/api/users/${id}`, {
+      await jwtaxios.delete(`http://localhost:3001/api/users/${id}`, {
         headers: { authorization: "Bearer " + user.accesstoken },
       });
       setSuccess(true);
@@ -37,7 +71,23 @@ function App() {
       setError(true);
     }
   };
-
+  const handleLogOut = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3001/api/logout",
+        {
+          token: user.refreshtoken,
+        },
+        {
+          headers: {
+            authorization: "Bearer " + user.accesstoken,
+          },
+        }
+      );
+      console.log(res.data);
+      setlogout(true);
+    } catch (error) {}
+  };
   return (
     <div className="container">
       {/* {console.log("this is user", user)} */}
@@ -64,6 +114,17 @@ function App() {
               User has been deleted successfully...
             </span>
           )}
+          <div>
+            <button onClick={() => handleLogOut()}>Log Out</button>
+            {logout && (
+              <span className="success">
+                User has been logged out successfully...
+              </span>
+            )}
+          </div>
+          <div>
+            <button onClick={() => refreshToken()}>Refresh token</button>
+          </div>
         </div>
       ) : (
         <div className="login">
